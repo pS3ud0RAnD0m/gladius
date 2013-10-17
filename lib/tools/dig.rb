a11f1beaa94f3d0704feadaaf4e68c28212c876a
pS3ud0RAnD0m <pS3ud0RAnD0m@users.noreply.github.com>
2013-10-17 10:07:27 -0400

    Refactor Dsniff

--- lib/tools/dig.rb
+++ lib/tools/dig.rb
@@ -3,10 +3,6 @@
 # Author:  p$3ud0R@nD0m
 # Version: 0.0.2
 
-require_relative '../helpers/sysinfo'
-require_relative 'tool'
-require_relative 'gpty'
-
 class DiG < Tool
   def initialize(title)
     @title = title
