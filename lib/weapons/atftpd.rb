dd7d0a41b71a137e666f1405eed64cfbd86cc212
pud0R@nD0m <maximushyde@gmail.com>
2013-10-30 18:43:57 -0400

    Refactor atftpd

--- lib/weapons/atftpd.rb
+++ lib/weapons/atftpd.rb
@@ -1,44 +1,65 @@
-#!/usr/bin/env ruby
-
 # Author:  p$3ud0R@nD0m
-# Version: 0.0.2
-
-require_relative '../helpers/sysinfo'
-require_relative 'weapon'
-require_relative 'gpty'
 
 class Atftpd < Weapon
-  def initialize(title)
+  def initialize(prev_menu, title)
+    @prev_menu = prev_menu
     @title = title
-    @@path = "dsniff"
+    @path = "atftpd"
+    @name = @path
   end
 
-  def header_atftpd
+###############################################################################
+# DRY methods
+###############################################################################
+  # Dry menu
+  def menu(run_method)
     header
-  end
-
-  # Setup a TFTP server
-  def setup
-    header_atftpd
-    puts
-
     # ID if IPtables is configed to drop anything
-    #puts "Checking IPtables' status."light_yellow
-    a = `iptables -L INPUT -nv |grep DROP`.chomp
-    unless a.empty?
-      puts "IPtables' INPUT chain is dropping.".yellow
-      b = `/sbin/iptables -C INPUT -p udp --dport 69 -j ACCEPT`
-      unless b.empty?
-        puts "b is not empty"
-        # ...
+    drop_status = `iptables -L INPUT -nv |grep DROP`.chomp
+    unless drop_status.empty?
+      tftp_rule_status = `iptables -L INPUT -nv |grep "udp dpt:69" |grep ACCEPT`
+      unless tftp_rule_status.empty?
+        puts "Inbound TFTP rule detected. The firewall looks good.".yellow
+        puts tftp_rule_status
+        config
       else
-        puts "Inbound TFTP rule detected. The firewall looks good."
+        puts "IPtables' INPUT chain is dropping.".yellow
+        puts "Inbound TFTP rule not detected. Please verify your firewall settings.".yellow
+        puts "There should be a rule similar to this:"
+        puts "/sbin/iptables -C INPUT -p udp --dport 69 -j ACCEPT"
       end
     else
-      puts "IPtables is not dropping. Consider configuring IPtables soon.".yellow
-      #atftpd --daemon --port 69 --bind-address 10.0.12.103 --logfile /var/log/atftpd.log /tmp/tftp
+      puts "IPtables is not dropping. Consider configuring IPtables soon."
+      config
     end
-    puts
-    KaliSupport.new("Configure Kali Services").menu
+  end
+
+  # Parse and exit
+  def clean_exit
+    exit_weapon
+  end
+
+###############################################################################
+# Run methods
+###############################################################################
+  # Setup a TFTP server
+  def config
+    cmd = @path + " --daemon --port 69 --logfile /var/log/atftpd.log /tmp/tftp"
+    run(cmd)
+    atftpd_status = `netstat -an |grep udp |grep 69`
+    if atftpd_status.empty?
+      cmd = "netstat -an |grep udp |grep 69"
+      run(cmd)
+      puts
+      puts "Hmmmm, the server doesn't appear to have started:".red
+      puts "Personally, I would try it again because I have seen it require a few attempts before.".yellow
+    else
+      cmd = "netstat -an |grep udp |grep 69"
+      run(cmd)
+      puts "The server is running:".light_yellow
+      puts
+      puts "You can shutdown the server with 'killall atftpd'.".light_yellow
+    end
+    clean_exit
   end
 end  
