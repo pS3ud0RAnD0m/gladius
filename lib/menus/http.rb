30a897d199f3bfdf4dbc1d7a86510a6924b8ef2e
pud0R@nD0m <maximushyde@gmail.com>
2014-05-21 10:41:34 -0400

    Add support for TestSSLServer

--- lib/menus/http.rb
+++ lib/menus/http.rb
@@ -6,19 +6,21 @@ class HTTP < Menu
   def menu
     header
     puts "Select an option:".light_yellow
-    puts "1.  ID supported SSL/TLS protcols and ciphers (SSLScan)"
-    puts "2.  ID supported HTTP methods (Nmap)"
-    puts "3.  ID common vulns (Nikto)"
-    puts "4.  Apache - Enum users (Apache-users)"
+    puts "1.  ID supported SSL/TLS protcols, ciphers, etc. (TestSSLServer)"
+    puts "2.  ID supported SSL/TLS protcols and ciphers (SSLScan)"
+    puts "3.  ID supported HTTP methods (Nmap)"
+    puts "4.  ID common vulns (Nikto)"
+    puts "5.  Apache - Enum users (Apache-users)"
     puts "88. Back"
     puts "99. Exit Gladius"
     
     selection = gets.to_i
     case selection
-    when 1 then SSLScan.new("HTTP", "SSLScan - ID Protcols and Ciphers").menu("common")
-    when 2 then GNmap.new("HTTP", "Nmap - ID Supported HTTP Methods").menu("script_http_methods")
-    when 3 then Nikto.new("HTTP", "Nikto - ID Common Web Vulns").menu("")
-    when 4 then ApacheUsers.new("HTTP", "Apache-users - Enum Users").menu("enum")
+    when 1 then TestSSLServer.new("HTTP", "TestSSLServer - ID Protcols and Ciphers").menu("basic")
+    when 2 then SSLScan.new("HTTP", "SSLScan - ID Protcols and Ciphers").menu("common")
+    when 3 then GNmap.new("HTTP", "Nmap - ID Supported HTTP Methods").menu("script_http_methods")
+    when 4 then Nikto.new("HTTP", "Nikto - ID Common Web Vulns").menu("")
+    when 5 then ApacheUsers.new("HTTP", "Apache-users - Enum Users").menu("enum")
     when 88 then GatherInfo.new("Information Gathering").menu
     when 99 then GExeption.new.exit_gladius
     else puts "Invalid selection.".red
---
+++ lib/weapons/testsslserver.rb
@@ -0,0 +1,67 @@
+# Author: p$3ud0R@nD0m
+
+class TestSSLServer < Weapon
+  def initialize(prev_menu, title)
+    @prev_menu = prev_menu
+    @title = title
+    @path = Path.get("thirdparty") + "TestSSLServer.jar"
+    @name = "testsslserver"
+    @cmd = ""
+    @stdn_hosts = []
+  end
+
+###############################################################################
+# User interaction methods
+###############################################################################
+  # Get target(s) and pass to execute method
+  def menu(run_method)
+    header
+    instruct_input_targets("ip")
+    while line = gets
+      @stdn_hosts << line.chomp
+    end
+    if @stdn_hosts.count == 0
+      puts "No hosts were input.".red
+      menu(run_method)
+    elsif @stdn_hosts.count == 1
+      puts "Targeting " + @stdn_hosts[0] + " ..."
+      execute(run_method)
+    else 
+      hosts_count = @stdn_hosts.count
+      puts "Targeting #{hosts_count} hosts ..."
+      execute(run_method)
+    end
+  rescue Interrupt
+    GExeption.new.exit_weapon("TestSSLServer", @prev_menu)
+  end
+
+# ttd_1: Post run, option 88 bounces out to Home menu, not Gather Information menu.
+  # Clean exit
+  def clean_exit
+    puts
+    if File.exist?(@out_file)
+      puts "Raw output can be found here:".yellow
+      puts @out_file
+    end
+    exit_weapon
+  end
+
+###############################################################################
+# Execution methods
+###############################################################################
+  # Execute run method
+  def execute(run_method)
+    @out_file = Path.get_out_file_txt(@name)
+    out_file = @out_file
+    @stdn_hosts.each do |host|
+      `echo "------------------------------" >>#{out_file}`
+      `echo "#{host}" >>#{out_file}`
+      `echo "------------------------------" >>#{out_file}`
+      case run_method
+      when "basic" then cmd = "java -jar " + @path + " #{host} |tee -a #{out_file}"
+      end
+      run(cmd)
+    end
+    clean_exit
+  end
+end  
