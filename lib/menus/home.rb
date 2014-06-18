3687e0869d1895c54d3ceece26037d27f67964e7
pud0R@nD0m <maximushyde@gmail.com>
2014-06-17 23:45:38 -0400

    Rename LICENSE.txt file

--- lib/menus/home.rb
+++ lib/menus/home.rb
@@ -3,7 +3,7 @@
 class Home
   def header_home
     puts "------------------------------"
-    puts "Home                 v0.0.2.20"
+    puts "Home                 v0.0.2.21"
     puts "------------------------------"
   end
 
