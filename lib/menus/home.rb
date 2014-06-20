02c1628d327340afaeee1ddae2691d298ff7c9bb
pud0R@nD0m <maximushyde@gmail.com>
2014-06-19 23:11:37 -0400

    Modify usage image

--- lib/menus/home.rb
+++ lib/menus/home.rb
@@ -3,7 +3,7 @@
 class Home
   def header_home
     puts "------------------------------"
-    puts "Home                 v0.0.2.26"
+    puts "Home                 v0.0.2.27"
     puts "------------------------------"
   end
 
