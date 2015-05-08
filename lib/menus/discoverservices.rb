3b34a4fb3594e9d0d4dceab741f8e167b212cce0
pS3ud0RAnD0m <pS3ud0RAnD0m@users.noreply.github.com>
2015-05-08 11:25:40 -0400

    Improve Nmap scans

--- lib/menus/discoverservices.rb
+++ lib/menus/discoverservices.rb
@@ -13,13 +13,13 @@ class DiscoverServices < Menu
     puts "5.  UDP top 1000 ports"
     puts "6.  TCP/UDP top 1000 ports"
     puts "Common:".yellow
-    puts "7.  TCP top 1000 ports with versioning"
-    puts "8.  UDP top 1000 ports with versioning"
-    puts "9.  TCP/UDP top 1000 ports with versioning"
+    puts "7.  TCP top 1000 ports + version + OS"
+    puts "8.  UDP top 1000 ports + version + OS"
+    puts "9.  TCP/UDP top 1000 ports + version + OS"
     puts "Detailed:".yellow
-    puts "10. TCP top 1000 ports with versioning and scripts"
-    puts "11. UDP top 1000 ports with versioning and scripts"
-    puts "12. TCP/UDP top 1000 ports with versioning and scripts"
+    puts "10. TCP top 1000 ports + version + OS + scripts"
+    puts "11. UDP top 1000 ports + version + OS + scripts"
+    puts "12. TCP/UDP top 1000 ports + version + OS + scripts"
     puts "Thorough:".yellow
     puts "13. TCP all ports"
     puts "14. UDP all ports"
@@ -37,12 +37,12 @@ class DiscoverServices < Menu
     when 4 then GNmap.new("DiscoverServices", "Nmap - TCP Top 1000 Ports").menu("tcp_quick")
     when 5 then GNmap.new("DiscoverServices", "Nmap - UDP Top 1000 Ports").menu("udp_quick")
     when 6 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP Top 1000 Ports").menu("tcp_udp_quick")
-    when 7 then GNmap.new("DiscoverServices", "Nmap - TCP Top 1000 Ports Versioning").menu("tcp_quick_v")
-    when 8 then GNmap.new("DiscoverServices", "Nmap - UDP Top 1000 Ports Versioning").menu("udp_quick_v")
-    when 9 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP Top 1000 Ports Versioning").menu("tcp_udp_quick_v")
-    when 10 then GNmap.new("DiscoverServices", "Nmap - TCP Top 1000 Ports with scripts").menu("tcp_quick_scripts")
-    when 11 then GNmap.new("DiscoverServices", "Nmap - UDP Top 1000 Ports with scripts").menu("udp_quick_scripts")
-    when 12 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP Top 1000 Ports with scripts").menu("tcp_udp_quick_scripts")
+    when 7 then GNmap.new("DiscoverServices", "Nmap - TCP top 1000 ports + version + OS").menu("tcp_quick_vo")
+    when 8 then GNmap.new("DiscoverServices", "Nmap - UDP top 1000 ports + version + OS").menu("udp_quick_vo")
+    when 9 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP top 1000 ports + version + OS").menu("tcp_udp_quick_vo")
+    when 10 then GNmap.new("DiscoverServices", "Nmap - TCP top 1000 ports + version + OS + scripts").menu("tcp_quick_vo_scripts")
+    when 11 then GNmap.new("DiscoverServices", "Nmap - UDP top 1000 ports + version + OS + scripts").menu("udp_quick_vo_scripts")
+    when 12 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP top 1000 ports + version + OS + scripts").menu("tcp_udp_quick_vo_scripts")
     when 13 then GNmap.new("DiscoverServices", "Nmap - TCP All Ports").menu("tcp_full")
     when 14 then GNmap.new("DiscoverServices", "Nmap - UDP All Ports").menu("udp_full")
     when 15 then GNmap.new("DiscoverServices", "Nmap - TCP/UDP All Ports").menu("tcp_udp_full")
